d27892ca6f31333648f8abd5f3eb651e84a83ec9
Matt James <mattjameskott@gmail.com>
2024-05-03 19:45:00 -0400

    Commenting exit and prints

--- network.py
+++ network.py
@@ -294,10 +294,10 @@ class Unsupervised_kpnet(nn.Module):
 
     def forward(self, pc):
         x = self.pointnet_encoder(pc.permute(0, 2, 1))   # [B x 1024 x 2048]
-        ptv3_dict = create_ptv3_dict(pc, pc, 0.05)
-        t = self.ptv3_encoder(ptv3_dict)
-        print(x.shape, t.feat.shape)
-        exit()
+        # ptv3_dict = create_ptv3_dict(pc, pc, 0.05)
+        # t = self.ptv3_encoder(ptv3_dict)
+        # print(x.shape, t.feat.shape)
+        # exit()
         # Down-sampling from 1024 to M key-points
         x = self.block1(x)          # [B x 512 x 2048]
         x = self.block2(x)          # [B x 256 x 2048]
