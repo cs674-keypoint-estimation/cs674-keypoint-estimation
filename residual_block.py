fb5c1a9294301dcd44372a4c68bc7422f430c850
Evan Young <44103787+EvYoung11@users.noreply.github.com>
2024-05-13 17:16:53 -0400

    Updated Res Block

--- residual_block.py
+++ residual_block.py
@@ -40,21 +40,21 @@ class residual_block_group_norm(nn.Module):
     def __init__(self, in_, out_):
         super(residual_block_group_norm, self).__init__()
         self.conv1 = nn.Conv1d(in_channels=in_, out_channels=in_,kernel_size=1)
-        self.instant_norm1 = nn.GroupNorm(num_features=in_)
+        self.group_norm1 = nn.GroupNorm(num_channels=10, num_features=in_)
         self.relu1 = nn.ReLU()
         self.conv2 = nn.Conv1d(in_channels=in_, out_channels=out_,kernel_size=1)
-        self.instant_norm2 = nn.GroupNorm(num_features=out_)
+        self.group_norm2 = nn.GroupNorm(num_channels=10, num_features=out_)
 
     def forward(self, input):
         x1 = self.conv1(input)
-        x1 = self.instant_norm1(x1)
+        x1 = self.group_norm1(x1)
         x1 = self.relu1(x1)
 
         x2 = self.conv2(input)
-        x2 = self.instant_norm2(x2)
+        x2 = self.group_norm2(x2)
 
         x3 = self.conv2(x1)
-        x3 = self.instant_norm2(x3)
+        x3 = self.group_norm2(x3)
 
         x3 += x2
 
@@ -69,21 +69,21 @@ class residual_block_layer_norm(nn.Module):
     def __init__(self, in_, out_):
         super(residual_block_group_norm, self).__init__()
         self.conv1 = nn.Conv1d(in_channels=in_, out_channels=in_,kernel_size=1)
-        self.instant_norm1 = nn.GroupNorm(num_features=in_)
+        self.layer_norm1 = nn.LayerNorm(num_features=[C,H,W])
         self.relu1 = nn.ReLU()
         self.conv2 = nn.Conv1d(in_channels=in_, out_channels=out_,kernel_size=1)
-        self.instant_norm2 = nn.GroupNorm(num_features=out_)
+        self.layer_norm2 = nn.LayerNorm(num_features=[C,H,W])
 
     def forward(self, input):
         x1 = self.conv1(input)
-        x1 = self.instant_norm1(x1)
+        x1 = self.layer_norm1(x1)
         x1 = self.relu1(x1)
 
         x2 = self.conv2(input)
-        x2 = self.instant_norm2(x2)
+        x2 = self.layer_norm2(x2)
 
         x3 = self.conv2(x1)
-        x3 = self.instant_norm2(x3)
+        x3 = self.layer_norm2(x3)
 
         x3 += x2
 
